ce82545eace8eaad76137658cf6f64ac2d293b37
PavelMax81 <mpv19810103@gmail.com>
2025-01-08 20:19:53 +0700

    Local variable "currentPath" was added to  main(). Field "currentPath" is deleted. Instruction for user is written to "Command.txt".

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
-    private static String currentPath = "";
     public static void main(String[] args) throws IOException {
-        currentPath = new java.io.File(".").getCanonicalPath();
+        String currentPath = new java.io.File(".").getCanonicalPath();
         String[] stringArgs = ArgStringParser.parseArgs(args);
         FileParser fileParser = new FileParser(currentPath, stringArgs);
         ArrayList<String> outputFiles = FileCreator.createOutputFiles(currentPath, fileParser);
